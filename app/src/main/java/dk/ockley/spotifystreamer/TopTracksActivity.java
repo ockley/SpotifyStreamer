7a8a1e8408da267d9bdc701d79c2dfd473f9abcc
ockley <vestergaard.karsten@gmail.com>
2015-06-10 23:52:07 +0200

    Changed from ActionBarActivity to AppCompatActivity

--- app/src/main/java/dk/ockley/spotifystreamer/TopTracksActivity.java
+++ app/src/main/java/dk/ockley/spotifystreamer/TopTracksActivity.java
@@ -1,12 +1,12 @@
 package dk.ockley.spotifystreamer;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TopTracksActivity extends ActionBarActivity {
+public class TopTracksActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
